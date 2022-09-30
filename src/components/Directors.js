b807f5aedc3d48f6ca95905c23f97cc5327c7005
Margaret Kenda <margaret.kenda@yahoo.com>
2022-09-30 19:18:26 +0300

    completed assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,19 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const eachdirector = directors.map((director) =>(
+    <div key={director.name}>
+      <h2>{director.name}</h2>
+      <ul>
+        {director.movies.map((movie) => (<li key={movie}>{movie}</li>))}
+      </ul>
+    </div>
+  ));
+  return (<div>
+    {/*{code here}*/}
+    <h1>Directors Page</h1>
+    {eachdirector}
+    </div>);
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const everymovie=movies.map((movie) =>(
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <p>{movie.time} min.</p>
+      <ul>
+        {movie.genres.map((genre)=>(<li key={genre}>{genre}</li>))}
+      </ul>
+    </div>
+  ));
+    return(<div>
+      <h1>Movies Page</h1>
+      {everymovie}
+    </div>);
+
 }
+  
+    
 
 export default Movies;
